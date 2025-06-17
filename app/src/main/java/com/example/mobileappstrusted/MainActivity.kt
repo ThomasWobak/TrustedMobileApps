cd4ec2a3fb077095fb7ba4f7acb3d5088aec5b4d
l1wobak <l1wobak@edu.aau.at>
2025-06-17 17:27:27 +0200

    todos

--- app/src/main/java/com/example/mobileappstrusted/MainActivity.kt
+++ app/src/main/java/com/example/mobileappstrusted/MainActivity.kt
@@ -10,19 +10,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.mobileappstrusted.ui.theme.MobileAppsTrustedTheme
 
+//TODO cybersecurity
+// ============================
+//TODO: Implement cutting of audio (Double check hash tree, check edit history if matching current index order)
+//TODO: Implement removing of audio (Encrypt with Password, append hash of unencrypted block, use hash of block in root hash checking if deleted)
+//TODO: Implement digital signature stuff
+//TODO: Implement validate Recording (Almost done --> need removing and cutting to validate properly)
+//TODO: Implement validate imported Audio
+// Lukas
+
+//TODO: Implement editing script with metadata and changes (user information)
+//TODO: Implement general metadata on recording (user information)
+// Thomas
+
+//TODO usability
+// ============================
 //TODO: Import audio from filesystem DONE
 //TODO: Be able to read .wav files DONE
 //TODO: Implement looking at audio DONE
 //TODO: Implement playback of audio DONE
-//TODO: Implement cutting of audio
-//TODO: Implement removing of audio
+
+//TODO: Improve cutting of audio (ease of use)
+//TODO: Improve removing of audio (granularity?, ease of use)
 //TODO: Implement menu in editing screen
-//TODO: Implement exporting of project
-//TODO: Implement adding hash value when cutting/removing
-//TODO: Implement hash tree to editing steps
-//TODO: Implement validate Recording
-//TODO: Implement going back using strg+z using hash tree
-//TODO: improve audio recording
+//TODO: Implement exporting of edited file
+//TODO: Implement going back using strg+z using temporary in memory editing script
+// Thomas
+
+
+//TODO Low priority
+// ============================
+//TODO: verify if added block headers are noticeable
+//TODO: audio watermark with metadata information
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
